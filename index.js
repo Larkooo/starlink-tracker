7c85713aa4381ec58d31c3cc5627072ff4143781
Larko <59736843+Larko00@users.noreply.github.com>
2020-07-02 21:48:59 -0400

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,14 @@ const app = express();
 
 app.use(express.json())
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
+
 var DATA_URL = "http://celestrak.com/NORAD/elements/starlink.txt";
 
 var set = 'ISS (ZARYA)\n' +
