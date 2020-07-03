f8382da1eccb170e0e58034d4a07a4f8b3dc2229
Larko <59736843+Larko00@users.noreply.github.com>
2020-07-03 05:25:35 -0400

    height

--- index.js
+++ index.js
@@ -109,7 +109,7 @@ app.get("/api/globe", (req, res) => {
     .then(resp => resp.json())
     .then(json => json.forEach(element => {
       let info = getSatelliteInfo(element.tleArr);
-      globeArr.push(info.lat, info.lng, info.range)
+      globeArr.push(info.lat, info.lng, info.height)
       //console.log(jsonArr)
     }))
   //res.send(jsonArr)
