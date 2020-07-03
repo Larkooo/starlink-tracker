5c4cc40437065e7a9cd4f6553ceaa33173e9c251
Larko <59736843+Larko00@users.noreply.github.com>
2020-07-03 05:33:23 -0400

    Update index.js

--- index.js
+++ index.js
@@ -109,7 +109,7 @@ app.get("/api/globe", (req, res) => {
     .then(resp => resp.json())
     .then(json => json.forEach(element => {
       let info = getSatelliteInfo(element.tleArr);
-      globeArr.push(info.lat, info.lng, info.height / 600)
+      globeArr.push(info.lat, info.lng, info.height / 800)
       //console.log(jsonArr)
     }))
   //res.send(jsonArr)
