a8c69da72e24b687115134f304143727883df14e
Larko <59736843+Larkooo@users.noreply.github.com>
2020-07-07 00:02:17 -0400

    Update index.js

--- index.js
+++ index.js
@@ -41,81 +41,38 @@ function exit( error ) {
 }
 
 app.get("/api/all", (req, res) => {
-    var req = http.get( DATA_URL, function( response ) {
+  var req = http.get(DATA_URL, function (response) {
+    if (response.statusCode !== 200)
+      return exit(
+        new Error(`HTTP ${response.statusCode} ${response.statusMessage}`)
+      );
 
-        if( response.statusCode !== 200 )
-          return exit( new Error( `HTTP ${response.statusCode} ${response.statusMessage}` ) )
-      
-        var start = Date.now()
-        var count = 0
-      
-        var tles = [];
+    var start = Date.now();
+    var count = 0;
 
-        var dt = response.pipe( new TLE.Parser() )
-          .once( 'error', exit )
-          .on( 'data', function( tle ) {
-            count++
-            tles.push(tle)
-          })
-          .once( 'finish', function() {
-            var time = Date.now() - start
-            var ops = count / ( time / 1000 )
-            res.send(tles)
-            console.log( 'Parser:', count, 'TLEs,', time + 'ms,', ops.toFixed(), 'op/s' )
-          })
-      
-    })
-});
+    var tles = [];
 
-var jsonArr = [];
-app.get("/api/geojson", (req, res) => {
-  fetch('http://localhost:'+ process.env.PORT +'/api/all')
-    .then(resp => resp.json())
-    .then(json => json.forEach(element => {
-      let tle = element.tleArr;
-      jsonArr.push({
-        name: element.name,
-        norad: element.number,
-        class: element.class,
-        id: element.id,
-        inclination: element.inclination,
-        motion: element.motion,
-        drag: element.drag,
-        ascension: element.ascension,
-        eccentricity: element.eccentricity,
-        perigee: element.perigee,
-        anomaly: element.anomaly,
-        revolution: element.revolution,
-        tleArray: tle,
-        date: element.date,
-        year: getEpochYear(tle),
-        day: getEpochDay(tle),
-        timestamp: getEpochTimestamp(tle),
-        orbitmodel: getOrbitModel(tle),
-        designator: getIntDesignatorYear(tle),
-        info: getSatelliteInfo(tle)
+    var dt = response
+      .pipe(new TLE.Parser())
+      .once("error", exit)
+      .on("data", function (tle) {
+        count++;
+        tles.push(tle);
       })
-      //console.log(jsonArr)
-    }))
-  //res.send(jsonArr)
-  res.send(GeoJSON.parse(jsonArr, {Point: ['info.lat', 'info.lng']}))
-  //res.send(jsonArr)
-  jsonArr = []
-});
-
-var globeArr = [];
-app.get("/api/globe", (req, res) => {
-  fetch('http://localhost:'+ process.env.PORT +'/api/all')
-    .then(resp => resp.json())
-    .then(json => json.forEach(element => {
-      let info = getSatelliteInfo(element.tleArr);
-      globeArr.push(info.lat, info.lng, info.height / 1600)
-      //console.log(jsonArr)
-    }))
-  //res.send(jsonArr)
-  res.send(globeArr)
-  //res.send(jsonArr)
-  globeArr = []
+      .once("finish", function () {
+        var time = Date.now() - start;
+        var ops = count / (time / 1000);
+        res.send(GeoJSON.parse(tles, {Point: ['info.lat', 'info.lng']}))
+        console.log(
+          "Parser:",
+          count,
+          "TLEs,",
+          time + "ms,",
+          ops.toFixed(),
+          "op/s"
+        );
+      });
+  });
 });
 
 app.listen(process.env.PORT);
